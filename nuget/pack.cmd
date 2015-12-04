acfcc1247e131336ddce87310631f7f06581ae54
Stefan Negritoiu <stefann@stefann.com>
2015-12-03 17:41:02 -0800

    bump nuget package to v1.2.0
    and include both AzureAD and MicrosoftOnline assemblies

--- nuget/pack.cmd
+++ nuget/pack.cmd
@@ -1,2 +1,3 @@
 msbuild /property:Configuration=Release ..\AzureAD-OAuth-Provider\AzureAD-OAuth-Provider.csproj
+msbuild /property:Configuration=Release ..\MicrosoftOnline-OAuth-Provider\MicrosoftOnline-OAuth-Provider.csproj
 nuget pack AzureAD-OAuth-Provider.nuspec
