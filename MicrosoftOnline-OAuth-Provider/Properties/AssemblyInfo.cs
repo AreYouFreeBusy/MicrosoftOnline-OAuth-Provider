d68149d0d2d6dfd3565d3623de2ec856e0f17f69
Stefan Negritoiu <stefan@freebusy.io>
2020-07-28 00:56:15 -0700

    bump package version to 1.5.3

--- MicrosoftOnline-OAuth-Provider/Properties/AssemblyInfo.cs
+++ MicrosoftOnline-OAuth-Provider/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Owin.Security.Providers.MicrosoftOnline")]
-[assembly: AssemblyCopyright("Copyright © 2017 Stefan Negritoiu (FreeBusy)")]
+[assembly: AssemblyCopyright("Copyright © Stefan Negritoiu (FreeBusy)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0")]
-[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyVersion("1.5.0")]
+[assembly: AssemblyFileVersion("1.5.0")]
